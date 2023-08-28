6070d676ffeb6c923bb7b49ea879070761a6acae
Samim-mohammad <mohammad.samim.habiby@tekschool.us>
2023-08-27 21:58:00 -0700

    new task 2 completed

---
+++ src/tek/git/tasks/Task2.java
@@ -0,0 +1,10 @@
+package tek.git.tasks;
+
+public class Task2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("I am doing well with GIT");
+	}
+
+}
