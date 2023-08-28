736999a726f036b2f09de5447aaae760e2080d15
Samim-mohammad <mohammad.samim.habiby@tekschool.us>
2023-08-27 18:12:08 -0700

    change from new branch

--- src/tek/git/project/SecondClassFromNewBranch.java
+++ src/tek/git/project/SecondClassFromNewBranch.java
@@ -3,7 +3,7 @@ package tek.git.project;
 public class SecondClassFromNewBranch {
 	
 	public static void main(String[] args) {
-		System.out.println("This is changes from my new branch");
+		System.out.println("This is gonna be my new changes in my new branch");
 		
 	}
 	
