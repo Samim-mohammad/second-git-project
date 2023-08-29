edca698472e57398dd00641ed9f0675112929731
Samim-mohammad <mohammad.samim.habiby@tekschool.us>
2023-08-28 17:59:35 -0700

    updating existing class

--- src/tek/git/project/SecondClassFromNewBranch.java
+++ src/tek/git/project/SecondClassFromNewBranch.java
@@ -5,6 +5,7 @@ public class SecondClassFromNewBranch {
 	public static void main(String[] args) {
 		System.out.println("This is gonna be my new changes in my new branch");
 		
+		System.out.println("This is update from new branch");
 	}
 	
 
